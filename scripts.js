c5571d6a9c49c804f56f693b93328fd6fa902d6b
atulpanda <atulpanda2006@gmail.com>
2025-12-06 15:37:17 +0530

    Change in theme

--- scripts.js
+++ scripts.js
@@ -1,16 +1,36 @@
-let btn = document.getElementById("submit")
-let names = document.getElementById("name")
-let mail = document.getElementById("email")
-let tel = document.getElementById("phone")
-let dates = document.getElementById("date")
+let btn = document.getElementById("submit");
+let names = document.getElementById("name");
+let mail = document.getElementById("email");
+let tel = document.getElementById("phone");
+let dates = document.getElementById("date");
 
+let dark = document.getElementById("theme");
+let body = document.getElementsByTagName("body");
 
-btn.addEventListener("click",function(){
-   if (names.value === '' && mail.value === "" && tel.value === '' && dates.value === '')
-    {
-        alert("Form is incomplete")
-    }
-    else{
-    alert("Form submitted .Thanks for visting 🙏")
-    }
-})
+body[0].className = "light";
+
+btn.addEventListener("click", function () {
+
+  if (names.value === '' && mail.value === "" && tel.value === '' && dates.value === '') {
+    alert("Form is incomplete");
+  } 
+  else {
+    alert("Form submitted. Thanks for visiting 🙏");
+  }
+});
+
+dark.addEventListener("click", function () {
+
+  if (body[0].className === "light") {
+
+    body[0].className = "dark";   // ONLY change class
+    dark.textContent = "Light Mode";
+  }
+
+  else {
+
+    body[0].className = "light";  // ONLY change class
+    dark.textContent = "Dark Mode";
+  }
+
+});
